f95bca4ee6afaa30053849d9b85fe07ff704d713
Bentleyli <670259582@qq.com>
2022-06-08 18:50:27 +0800

    1、商品详情页轮播图实现

--- pages/goods-detail/goods-detail.vue
+++ pages/goods-detail/goods-detail.vue
@@ -1,6 +1,10 @@
 <template>
-	<view>
-		商品详情1
+	<view class="home">
+		<swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000">
+			<swiper-item v-for="item in bannerImages">
+				<image :src="imgUrl" mode=""></image>
+			</swiper-item>
+		</swiper>
 	</view>
 </template>
 
@@ -8,14 +12,20 @@
 	export default {
 		data() {
 			return {
-				
+				bannerImages: [],
+				imgUrl: "https://t7.baidu.com/it/u=4272803060,4157549139&fm=193&f=GIF"
 			}
 		},
 		methods: {
-			
+			async getGoodsBannerImages(id) {
+				const res = await this.$myRequest({
+					url: '/api/getthumimages/' + id
+				})
+				this.bannerImages = res.data.message
+			}
 		},
 		onLoad(options) {
-			console.log(options);
+			this.getGoodsBannerImages(options.id)
 		}
 	}
 </script>
