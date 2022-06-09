f213457a41aed8374852590e1ac7cead0e70f727
Bentleyli <670259582@qq.com>
2022-06-09 18:59:55 +0800

    商品详情数据获取

--- pages/goods-detail/goods-detail.vue
+++ pages/goods-detail/goods-detail.vue
@@ -7,12 +7,16 @@
 		</swiper>
 		<view class="box1">
 			<view class="price">
-				<text>￥6888</text>
-				<text>￥8888</text>
+				<text>￥{{goodsInfo.sell_price}}</text>
+				<text>￥{{goodsInfo.market_price}}</text>
 			</view>
 			<view class="goods_name">
-				iphone 13
+				{{goodsInfo.title}}
 			</view>
+		</view>
+		 <!-- content区域 -->
+		<view class="content">
+			<rich-text :nodes="goodsDesc.content"></rich-text>
 		</view>
 	</view>
 </template>
@@ -23,7 +27,9 @@
 			return {
 				bannerImages: [],
 				id: 0,
-				imgUrl: "https://t7.baidu.com/it/u=4272803060,4157549139&fm=193&f=GIF"
+				imgUrl: "https://t7.baidu.com/it/u=4272803060,4157549139&fm=193&f=GIF",
+				goodsInfo: {},
+				goodsDesc: {}
 			}
 		},
 		methods: {
@@ -32,37 +38,59 @@
 					url: '/api/getthumimages/' + this.id
 				})
 				this.bannerImages = res.data.message
+			},
+			async getGoodsInfo() {
+				const res = await this.$myRequest({
+					url: '/api/goods/getinfo/' + this.id
+				})
+				this.goodsInfo = res.data.message[0]
+			},
+			async getGoodsDesc() {
+				const res = await this.$myRequest({
+					url: '/api/goods/getdesc/' + this.id
+				})
+				this.goodsDesc = res.data.message[0]
 			}
 		},
 		onLoad(options) {
 			this.id = options.id
 			this.getGoodsBannerImages()
+			this.getGoodsInfo()
+			this.getGoodsDesc()
 		}
 	}
 </script>
 
 <style lang="scss">
-	.goods_detail{
-		swiper{
+	.goods_detail {
+		swiper {
 			height: 700rpx;
-			image{
+
+			image {
 				width: 100%;
 				height: 100%;
 			}
 		}
-		.box1{
+
+		.box1 {
 			padding: 10px;
-			.price{
+
+			.price {
 				font-size: 35rpx;
 				color: $shop-color;
-				text:nth-child(2){
+
+				text:nth-child(2) {
 					color: #ccc;
 					font-size: 28rpx;
 				}
 			}
-			.goods_name{
+
+			.goods_name {
 				font-size: 35rpx;
 			}
+		}
+		.content{
+			margin: 10rpx 10rpx;
 		}
 	}
 </style>
